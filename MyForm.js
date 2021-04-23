3bace80be91931d504a1d44a6565ba02463d5591
Nicolas TORRE <n.torre@e2time.com>
2021-04-23 10:28:46 +0200

    Second step: Simple form with JSX and no style

--- MyForm.js
+++ MyForm.js
@@ -2,15 +2,12 @@ import React from "react";
 import { View, TextInput, Text, Button } from "react-native";
 
 function MyForm(props) {
-  return React.createElement(
-    View,
-    {},
-    React.createElement(TextInput, { placeholder: "Euro" }),
-    React.createElement(Button, {
-      title: "Convert",
-      onPress: () => {},
-    }),
-    React.createElement(Text, [], ["... bitoin"])
+  return (
+    <View>
+      <TextInput placeholder="Euro" />
+      <Button title="Convert" onPress={() => {}} />
+      <Text>... Bitcoin</Text>
+    </View>
   );
 }
 
